fcbd4b6a9f9890da2d4c7e0a7cc818770ae98835
Rikuto Sueta <rikutonsicloud@gmail.com>
2020-04-21 17:01:41 +0900

    Add SpeechToTextConverter class

--- app/src/main/java/com/example/ohukurorecipe/SpeechToTextConverter.kt
+++ app/src/main/java/com/example/ohukurorecipe/SpeechToTextConverter.kt
@@ -2,11 +2,20 @@ package com.example.ohukurorecipe
 
 import android.content.Context
 import com.google.auth.oauth2.GoogleCredentials
+import com.google.cloud.speech.v1.RecognitionConfig
+import com.google.cloud.speech.v1.SpeechGrpc
+import com.google.cloud.speech.v1.StreamingRecognitionConfig
+import com.google.cloud.speech.v1.StreamingRecognizeRequest
+import com.google.protobuf.ByteString
 import io.grpc.ManagedChannelBuilder
+import io.grpc.auth.MoreCallCredentials
+import io.grpc.stub.StreamObserver
+
 
 class SpeechToTextConverter(private val context: Context) {
-
-    private fun connectToServer() : Unit {
+    private lateinit var speechStub : SpeechGrpc.SpeechStub
+    private lateinit var observer: StreamObserver<StreamingRecognizeRequest>
+    fun connectToServer() : Unit {
         val resources = context.resources
         val googleCredentials = GoogleCredentials.fromStream(
             resources.openRawResource(R.raw.api_key)
@@ -14,10 +23,39 @@ class SpeechToTextConverter(private val context: Context) {
 
         val managedChannelBuilder = ManagedChannelBuilder.forTarget("search.googleapis.com")
         val managedChannel = managedChannelBuilder.build()
-        //val speechStub = SpeechGrpc.newStub(managedChannel)
-        //    .withCall
+        speechStub = SpeechGrpc.newStub(managedChannel)
+            .withCallCredentials(MoreCallCredentials.from(googleCredentials))
     }
-    fun convert(): Unit {
 
+    fun startStreaming() : Unit {
+        observer = speechStub.streamingRecognize(MyObserver())
+
+        val config = StreamingRecognitionConfig.newBuilder()
+            .setConfig(
+                RecognitionConfig.newBuilder()
+                    .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+                    .setSampleRateHertz(context.resources.getInteger(R.integer.sample_rate_hertz))
+                    .setLanguageCode(context.resources.getString(R.string.language_code))
+                    .build()
+            )
+            .setSingleUtterance(true)
+            .setInterimResults(true)
+            .build()
+
+        observer.onNext(
+            StreamingRecognizeRequest.newBuilder()
+                .setStreamingConfig(config)
+                .build()
+        )
+    }
+    fun convert(fragment: ByteString): Unit {
+        observer.onNext(
+            StreamingRecognizeRequest.newBuilder()
+                .setAudioContent(fragment)
+                .build()
+        )
+    }
+    fun endStreaming(): Unit {
+        observer.onCompleted()
     }
 }
